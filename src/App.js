b068b650edc68bd6a614b0156491e2f291425536
e.rozhko <e.rozhko@admins-iMac.local>
2017-11-28 18:44:21 +0200

    add - react-fullpage

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
+import {SectionsContainer, Section, Header, Footer} from 'react-fullpage';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+    let options = {
+      sectionClassName:     'section',
+      anchors:              ['sectionOne', 'sectionTwo', 'sectionThree'],
+      scrollBar:            false,
+      navigation:           true,
+      verticalAlign:        false,
+      arrowNavigation:      true,
+      scrollingSpeed: 400
+    };
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Header>
+          <a href="#sectionOne">Section One</a>
+          <a href="#sectionTwo">Section Two</a>
+          <a href="#sectionThree">Section Three</a>
+        </Header>
+        <Footer>
+          <a href="">Dcoumentation</a>
+          <a href="">Example Source</a>
+          <a href="">About</a>
+        </Footer>
+        <SectionsContainer className="container" {...options}>
+          <Section className="custom-section" verticalAlign="true" color="#69D2E7">Page 1</Section>
+          <Section color="#A7DBD8">Page 2</Section>
+          <Section color="#E0E4CC">Page 3</Section>
+        </SectionsContainer>
       </div>
     );
   }
