36c788c370b4f94ae857697940a3a12f53e16fd4
e.rozhko <e.rozhko@admins-iMac.local>
2017-11-28 19:35:51 +0200

    improve - menu component styles

--- src/components/MainMenu/MainMenu.js
+++ src/components/MainMenu/MainMenu.js
@@ -3,7 +3,7 @@ import './MainMenu.css';
 
 const MainMenu = () => {
 	return (
-		<ul>
+		<ul className="main-menu">
 			<li><a href="#sectionOne">Section One</a></li>
 			<li><a href="#sectionTwo">Section Two</a></li>
 			<li><a href="#sectionThree">Section Three</a></li>
