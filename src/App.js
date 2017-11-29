8e7f41cf4918550ef8d68296ac58f5ba9ea1644b
e.rozhko <e.rozhko@admins-iMac.local>
2017-11-29 15:48:45 +0200

    add - slider component choose function + app state

--- src/App.js
+++ src/App.js
@@ -7,6 +7,30 @@ import MainMenu from './components/MainMenu/MainMenu'
 import Slider from './components/Slider/Slider'
 
 class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      catalog: {
+        items: [
+          {name: 'AAAAAA'},
+          {name: 'BB'},
+          {name: 'CCC'},
+          {name: 'DDDDD'},
+          {name: 'EEE'}
+        ],
+        currentIndex: 0
+      }
+    }
+    this.chooseItem = this.chooseItem.bind(this)
+  }
+
+  chooseItem(e, i){
+    let catalog = {...this.state.catalog}
+    catalog.currentIndex = i
+
+    this.setState({catalog})
+  }
+
   render() {
     let options = {
       sectionClassName:     'section',
@@ -17,6 +41,7 @@ class App extends React.Component {
       arrowNavigation:      true,
       scrollingSpeed: 400
     };
+    const {catalog} = this.state
 
     return (
       <div>
@@ -29,7 +54,7 @@ class App extends React.Component {
         <SectionsContainer className="container" {...options}>
           <Section className="custom-section" verticalAlign="true" color="#69D2E7">Page 1</Section>
           <Section color="#A7DBD8">
-            <Slider />
+            <Slider catalog={catalog} chooseItem={this.chooseItem} />
           </Section>
           <Section color="#E0E4CC">Page 3</Section>
         </SectionsContainer>
--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -2,9 +2,20 @@ import React from 'react'
 
 class Slider extends React.Component {
 	render(){
+		let currentIndex = this.props.catalog.currentIndex
+		let current = this.props.catalog.items[currentIndex]
+
 		return(
 			<div>
 				<h2>Slider</h2>
+				<h4>{JSON.stringify(current)}</h4>
+				<ul>
+					{this.props.catalog.items.map((item, i)=>{
+						return(
+							<li key={i} onClick={(e) => this.props.chooseItem(e, i)}>{item.name}</li>
+						)
+					})}
+				</ul>
 			</div>
 		)
 	}
