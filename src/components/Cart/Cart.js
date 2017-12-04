0b7a33085d54c81f918df327e34c291fe2d37dd2
e.rozhko <e.rozhko@admins-iMac.local>
2017-12-04 18:11:22 +0200

    add - new modals by reactstrap

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import ReactModal from 'react-modal'
+import { Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import _ from 'lodash'
 
 import './Cart.css';
 import {Icon} from 'react-fa'
-
-ReactModal.setAppElement('#root');
 
 class Cart extends React.Component {
 	render(){
@@ -13,56 +11,42 @@ class Cart extends React.Component {
 		const counter = (cart.items.length) ? _.sumBy(cart.items, 'quantity') : 0
 		const total = (cart.items.length) ? _.sumBy(cart.items, i => i.quantity*i.price) : 0
 
-		const modalStyle = {
-			overlay: {
-				backgroundColor: 'rgba(231, 220, 210, 0.4)'
-			},
-			content: {
-				background: '#425351'
-			}
-		}
-
 		return(
 			<div className="text-right my-2 mx-4">
 				<p><a onClick={this.props.toggleCartModal}>Корзина ({counter})</a></p>
 
-				<ReactModal 
-		           isOpen={cart.opened}
-		           contentLabel="Корзина"
-		           style={modalStyle}
-		        >
-		        	<div className="container">
-		        		<div className="row align-items-top">
-		        			<div className="col-sm-12 text-right">
-		        				<a onClick={this.props.toggleCartModal}><Icon name="times" size="2x" /></a>
-		        			</div>
-		        		</div>
-			        	<div className="row align-items-center">
-				        	<div className="col-md-6 col-sm-12">
-				        		
-				        	</div>
-				        	<div className="col-md-6 col-sm-12 align-self-center">
-					        	{cart.items.length>0 ? (
-					        		<div>
-							        	<ul>
-							        		{cart.items.map((item, i) => {
-							        			return(
-							        				<li key={i}>
-							        					{item.name} ({item.quantity} по {item.price} грн.) 
-							        					<a onClick={(e) => this.props.removeItem(e, i)}>X</a>
-							        				</li>
-							        			)
-							        		})}
-							        	</ul>
-							        	<h4>{total} грн.</h4>
-						        	</div>
-						        ) : (
-						        	<h2>Корзина пуста</h2>
-						        )}
-						    </div>
-						</div>
+				<Modal isOpen={cart.opened} size="lg" toggle={this.toggle} className={this.props.className}>
+		          <ModalHeader toggle={this.props.toggleCartModal}>Modal title</ModalHeader>
+		          <ModalBody>
+		        	<div className="row">
+			        	<div className="col-md-6 col-sm-12">
+			        		
+			        	</div>
+			        	<div className="col-md-6 col-sm-12 align-self-center">
+				        	{cart.items.length>0 ? (
+				        		<div>
+						        	<ul>
+						        		{cart.items.map((item, i) => {
+						        			return(
+						        				<li key={i}>
+						        					{item.name} ({item.quantity} по {item.price} грн.) 
+						        					<a onClick={(e) => this.props.removeItem(e, i)}>X</a>
+						        				</li>
+						        			)
+						        		})}
+						        	</ul>
+						        	<h4>{total} грн.</h4>
+					        	</div>
+					        ) : (
+					        	<h2>Корзина пуста</h2>
+					        )}
+					    </div>
 					</div>
-        		</ReactModal>
+		          </ModalBody>
+		          <ModalFooter>
+
+		          </ModalFooter>
+		        </Modal>
 			</div>
 		)
 	}
