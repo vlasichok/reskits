aa10ccb1e8fa72eefadfc51636a2dd0da81b7308
e.rozhko <e.rozhko@admins-iMac.local>
2017-12-04 15:24:25 +0200

    add - Cart total cost count

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -7,27 +7,31 @@ ReactModal.setAppElement('#root');
 class Cart extends React.Component {
 	render(){
 		const cart = this.props.cart
-		const total = (cart.items.length) ? _.sumBy(cart.items, 'quantity') : 0
+		const counter = (cart.items.length) ? _.sumBy(cart.items, 'quantity') : 0
+		const total = (cart.items.length) ? _.sumBy(cart.items, i => i.quantity*i.price) : 0
 
 		return(
 			<div className="text-right my-2 mx-4">
-				<p><a onClick={this.props.toggleCartModal}>Корзина ({total})</a></p>
+				<p><a onClick={this.props.toggleCartModal}>Корзина ({counter})</a></p>
 
 				<ReactModal 
 		           isOpen={cart.opened}
 		           contentLabel="Корзина"
 		        >
 		        	{cart.items.length>0 ? (
-			        	<ul>
-			        		{cart.items.map((item, i) => {
-			        			return(
-			        				<li key={i}>
-			        					{item.name} - {item.quantity} 
-			        					<a onClick={(e) => this.props.removeItem(e, i)}>X</a>
-			        				</li>
-			        			)
-			        		})}
-			        	</ul>
+		        		<div>
+				        	<ul>
+				        		{cart.items.map((item, i) => {
+				        			return(
+				        				<li key={i}>
+				        					{item.name} ({item.quantity} по {item.price} грн.) 
+				        					<a onClick={(e) => this.props.removeItem(e, i)}>X</a>
+				        				</li>
+				        			)
+				        		})}
+				        	</ul>
+				        	<h4>{total} грн.</h4>
+			        	</div>
 			        ) : (
 			        	<h2>Корзина пуста</h2>
 			        )}
