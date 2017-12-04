7e740dec96dcb3571c1ebe7ce3d26f128d4535ed
e.rozhko <e.rozhko@admins-iMac.local>
2017-12-04 19:28:02 +0200

    add - burger for mobile menu

--- src/components/MainMenu/MainMenu.js
+++ src/components/MainMenu/MainMenu.js
@@ -1,15 +1,18 @@
 import React from 'react';
+
 import './MainMenu.css';
+import {Icon} from 'react-fa'
 
 const MainMenu = () => {
 	return (
 		<div className="text-right pull-right my-4">
-			<ul className="main-menu">
+			<ul className="main-menu d-none d-sm-block">
 				<li><a href="#main">Main</a></li>
 				<li><a href="#catalog">Catalog</a></li>
 				<li><a href="#test">Test</a></li>
 				<li><a href="#about">About</a></li>
 			</ul>
+			<a className="d-block d-sm-none"><Icon name="bars" size="2x" /></a>
 		</div>
 	)
 }
