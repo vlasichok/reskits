49d720b1d71f0b41a62701b5d47473d1c91fd7cf
e.rozhko <e.rozhko@admins-iMac.local>
2017-12-04 18:50:24 +0200

    add - buttons on homepage

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,14 @@
 import React from 'react'
 
 const Home = () => {
-	return( 
-		<h2 className="text-center">Какой-то крутой слоган будет тут, пока не очень ясно какой, но какой-то будет.</h2>
+	return(
+		<div className="text-center">
+			<h2>Какой-то крутой слоган будет тут, пока не очень ясно какой, но какой-то будет.</h2>
+			<div>
+				<a href='#catalog'><button className="btn btn-default mx-1">Каталог</button></a>
+				<a href='#test'><button className="btn btn-default mx-1">Подобрать аптечку</button></a>
+			</div>
+		</div>
     )
 }
 export default Home
