52ae77748b5328e7c702837040e494be1b42fc93
Evhen <evhen.rozhko@gmail.com>
2017-12-05 18:47:01 +0200

    add - slider current item view

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -9,12 +9,19 @@ class Slider extends React.Component {
 
 		return(
 			<div className="container slider">
-				<h3>
-					{current.name}
-					<button className="mx-3" onClick={(e) => this.props.addItem(e, current)}>Add</button>
-				</h3>
-				<div>
-					{current.descr.split('\n').map((paragraph, i) => <p key={i}>{paragraph}</p>)}
+				<div className="current-item row">
+					<div className="col-6">
+						<h1 className="text-right"><Icon name="medkit" size="2x" className="p-3 m-3" /></h1>
+					</div>
+					<div className="col-6">
+						<h3 className="name">
+							{current.name}
+							<button className="mx-3" onClick={(e) => this.props.addItem(e, current)}>Add</button>
+						</h3>
+						<div className="descr">
+							{current.descr.split('\n').map((paragraph, i) => <p key={i}>{paragraph}</p>)}
+				        </div>
+			        </div>
 		        </div>
 				<div className="items mt-2">
 					{this.props.catalog.items.map((item, i)=>{
