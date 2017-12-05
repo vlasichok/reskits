409bfbae00ae6e7aede459b2397d7544546e1380
Evhen <evhen.rozhko@gmail.com>
2017-12-05 19:16:11 +0200

    improve - Home component view

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Home = () => {
 	return(
-		<div className="text-center">
+		<div className="container text-center">
 			<h2>Какой-то крутой слоган будет тут, пока не очень ясно какой, но какой-то будет.</h2>
-			<div>
-				<a href='#catalog'><button className="btn btn-default mx-1">Каталог</button></a>
-				<a href='#test'><button className="btn btn-default mx-1">Подобрать аптечку</button></a>
+			<div className="mt-3">
+				<a href='#catalog'><button className="btn btn-default mr-3">Смотреть каталог</button></a>
+				<a href='#test'><button className="btn btn-default">Подобрать аптечку</button></a>
 			</div>
 		</div>
     )
