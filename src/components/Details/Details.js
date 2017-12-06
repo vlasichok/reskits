3dcb2ffc6655384c2b784d71ce69e88431bb7d42
Evhen <evhen.rozhko@gmail.com>
2017-12-06 14:07:07 +0200

    improve - Details slide adaptiveness

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -13,38 +13,54 @@ const Details = () => {
 				<Accordion>
 				    <AccordionItem expanded>
 				        <AccordionItemTitle>
-				        	<div className="pull-left mx-2 mt-1"><Icon name="plus" size="2x" /></div>
-				            <div className="title"><h3>Созданы для города</h3></div>
+				        	<div className="row mt-2">
+				            	<div className="icon col-2 col-sm-1 text-center"><Icon name="plus" size="2x" /></div>
+				            	<div className="title col-10 col-sm-11"><h3>Созданы для города</h3></div>
+				        	</div>
 				        </AccordionItemTitle>
 				        <AccordionItemBody>
-				            <p className="ml-4 pl-3">Reskits предназначены для использования в городской среде.</p>
+				        	<div className="row">
+				            	<div className="descr col-10 offset-2 col-sm-11 offset-sm-1"><p>Reskits предназначены для использования в городской среде.</p></div>
+				        	</div>
 				        </AccordionItemBody>
 				    </AccordionItem>
 				    <AccordionItem>
 				        <AccordionItemTitle>
-				            <div className="pull-left mx-2 mt-1"><Icon name="plus" size="2x" /></div>
-				            <div className="title"><h3>Удобно хранить и транспортировать</h3></div>
+				        	<div className="row mt-2">
+				            	<div className="icon col-2 col-sm-1 text-center"><Icon name="plus" size="2x" /></div>
+				            	<div className="title col-10 col-sm-11"><h4>Удобно хранить и транспортировать</h4></div>
+				        	</div>
 				        </AccordionItemTitle>
 				        <AccordionItemBody>
-				            <p className="ml-4 pl-3">Reskits представлены в двух видах упаковки: в вакуумной и функциональных чехлах из Cordura 1000D.</p>
+				        	<div className="row">
+				            	<div className="descr col-10 offset-2 col-sm-11 offset-sm-1"><p>Reskits представлены в двух видах упаковки: в вакуумной и функциональных чехлах из Cordura 1000D.</p></div>
+				        	</div>
 				        </AccordionItemBody>
 				    </AccordionItem>
 				    <AccordionItem>
 				        <AccordionItemTitle>
-				        	<div className="pull-left mx-2 mt-1"><Icon name="plus" size="2x" /></div>
-				            <div className="title"><h3>Правильное наполнение</h3></div>
+				        	<div className="row mt-2">
+				            	<div className="icon col-2 col-sm-1 text-center"><Icon name="plus" size="2x" /></div>
+				            	<div className="title col-10 col-sm-11"><h3>Правильное наполнение</h3></div>
+				        	</div>
 				        </AccordionItemTitle>
 				        <AccordionItemBody>
-				            <p className="ml-4 pl-3">При выборе снаряжения использовались международные стандарты первой помощи и практический опыт спасения жизни.</p>
+				        	<div className="row">
+				            	<div className="descr col-10 offset-2 col-sm-11 offset-sm-1"><p>При выборе снаряжения использовались международные стандарты первой помощи и практический опыт спасения жизни.</p></div>
+				        	</div>
 				        </AccordionItemBody>
 				    </AccordionItem>
 				    <AccordionItem>
 				        <AccordionItemTitle>
-				        	<div className="pull-left mx-2 mt-1"><Icon name="plus" size="2x" /></div>
-				            <div className="title"><h3>Жизненно-важный подарок</h3></div>
+				        	<div className="row mt-2">
+				            	<div className="icon col-2 col-sm-1 text-center"><Icon name="plus" size="2x" /></div>
+				            	<div className="title col-10 col-sm-11"><h3>Жизненно-важный подарок</h3></div>
+				        	</div>
 				        </AccordionItemTitle>
 				        <AccordionItemBody>
-				            <p className="ml-4 pl-3">Мы верим, что подарить Reskits значит проявить заботу о жизни и здоровье близких людей и коллег.</p>
+				        	<div className="row">
+				            	<div className="descr col-10 offset-2 col-sm-11 offset-sm-1"><p>Мы верим, что подарить Reskits значит проявить заботу о жизни и здоровье близких людей и коллег.</p></div>
+				        	</div>
 				        </AccordionItemBody>
 				    </AccordionItem>
 				</Accordion>
