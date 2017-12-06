03dbb08af38101c643711ac98aec3a1c16362f7d
Evhen <evhen.rozhko@gmail.com>
2017-12-06 16:24:56 +0200

    add - slider current item image

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -9,11 +9,11 @@ class Slider extends React.Component {
 
 		return(
 			<div className="container slider">
-				<div className="current-item row">
-					<div className="col-lg-6 col-md-4 col-sm-3 col-xs-12">
-						<h1 className="text-right"><Icon name="medkit" size="2x" className="p-3 m-3" /></h1>
+				<div className="current-item row mb-lg-4">
+					<div className="col-lg-3 offset-lg-2 col-md-4 col-sm-3 col-xs-12">
+						<img className="image" src="/img/UK1.jpg" />
 					</div>
-					<div className="col-lg-5 col-md-8 col-sm-9 col-xs-12">
+					<div className="col-lg-6 col-md-8 col-sm-9 col-xs-12">
 						<h3 className="name">
 							{current.name}
 							<button className="btn btn-light btn-sm mx-3" onClick={(e) => this.props.addItem(e, current)}>В корзину</button>
