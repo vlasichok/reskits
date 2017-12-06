84e6ffb3f8a1b5c4a71dbbd29b7887a8c0ea2ea0
Evhen <evhen.rozhko@gmail.com>
2017-12-06 11:50:03 +0200

    improve - homepage buttons margins

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,9 +4,9 @@ const Home = () => {
 	return(
 		<div className="container text-center">
 			<h2>Какой-то крутой слоган будет тут, пока не очень ясно какой, но какой-то будет.</h2>
-			<div className="mt-3">
-				<a href='#catalog'><button className="btn btn-light mr-3">Смотреть каталог</button></a>
-				<a href='#test'><button className="btn btn-light">Подобрать аптечку</button></a>
+			<div className="mt-3 text-center">
+				<a href='#catalog'><button className="btn btn-light m-1">Смотреть каталог</button></a>
+				<a href='#test'><button className="btn btn-light m-1">Подобрать аптечку</button></a>
 			</div>
 		</div>
     )
