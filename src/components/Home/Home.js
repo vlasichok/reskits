cc5e763418f72e4216388ba70a9e53d32a753f21
Evhen <evhen.rozhko@gmail.com>
2017-12-08 15:16:27 +0200

    add - homepage links decoration

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,8 +5,8 @@ const Home = () => {
 		<div className="container text-center">
 			<h2>Какой-то крутой слоган будет тут, пока не очень ясно какой, но какой-то будет.</h2>
 			<div className="mt-3 text-center">
-				<a href='#catalog'><button className="btn btn-light m-1">Смотреть каталог</button></a>
-				<a href='#test'><button className="btn btn-light m-1">Подобрать аптечку</button></a>
+				<a href='#catalog' className="no-decoration"><button className="btn btn-light m-1">Смотреть каталог</button></a>
+				<a href='#test' className="no-decoration"><button className="btn btn-light m-1">Подобрать аптечку</button></a>
 			</div>
 		</div>
     )
