e631bd3cf9d406e4c7df6f3e350dec9d6cc7edac
Evhen <evhen.rozhko@gmail.com>
2017-12-14 00:31:15 +0200

    improve - Test component style

--- src/components/Test/Test.js
+++ src/components/Test/Test.js
@@ -11,30 +11,33 @@ class Test extends React.Component {
 
 		return(
 			<div className="container test">
-				{finished ? (
-					<div className="text-center mb-2">
-						<h4>
-							{ winner.itemIndex !== undefined && <span>Вам подойдет </span>} 
-							{winner.name}
-						</h4>
-						<p>{winner.descr}</p>
-						{ winner.itemIndex !== undefined &&
-							<button className="btn btn-light m-1" onClick={(e)=> this.props.goToResult(e, winner.itemIndex)}>Узнать подробнее</button>
-						}
+				<div className="row">
+					<div className="col-10 offset-1 text-center">
+						{finished ? (
+							<div>
+								<h4>
+									{ winner.itemIndex !== undefined && <span>Вам подойдет </span>} 
+									{winner.name}
+								</h4>
+								{ winner.itemIndex !== undefined &&
+									<button className="btn btn-light m-1" onClick={(e)=> this.props.goToResult(e, winner.itemIndex)}>Узнать подробнее</button>
+								}
+							</div>
+						) : (
+							<div>
+								<h3 className="question mb-3">{currentQuestion.questionText}</h3>
+								<ul className="answers p-0 m-0">
+									{currentQuestion.answers.map((answer, i) => {
+										return(
+											<li key={i}><button className="btn btn-light m-1" onClick={(e) => this.props.giveAnswer(e, answer)}>{answer.answerText}</button></li>
+										)
+									})}
+								</ul>
+							</div>
+						)}
 					</div>
-				) : (
-					<div className="text-center">
-						<h3>{currentQuestion.questionText}</h3>
-						<ul className="answers p-0">
-							{currentQuestion.answers.map((answer, i) => {
-								return(
-									<li key={i}><button className="btn btn-light m-1" onClick={(e) => this.props.giveAnswer(e, answer)}>{answer.answerText}</button></li>
-								)
-							})}
-						</ul>
-					</div>
-				)}
-				<p className="text-center"><Icon name="refresh" /><a onClick={(e) => this.props.restartTest(e)} className="ml-2">Начать заново</a></p>
+				</div>
+				<p className="text-center mt-2"><Icon name="refresh" /><a onClick={(e) => this.props.restartTest(e)} className="ml-2">Начать заново</a></p>
 			</div>
 		)
 	}
