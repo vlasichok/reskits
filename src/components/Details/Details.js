85ff089374600d988aa73747b8b4dad63a8e26d5
Evhen <evhen.rozhko@gmail.com>
2017-12-14 00:06:12 +0200

    improve - Details component view (headings, text and adaptiveness)

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -11,16 +11,16 @@ const Details = (props) => {
 			<div className="row">
 				{props.items.map((item, i) => {
 					return(
-					    <div key={i} className="col-12 col-md-10 offset-md-1">
+					    <div key={i} className="col-12 col-lg-10 offset-lg-1 mt-2">
 				        	<div className="row mt-2">
-				            	<div className="icon col-2 col-sm-3 text-center text-md-right"><Icon name={item.icon} size="2x" /></div>
-				            	<div className="title col-10 col-sm-8">
-				            		<h3><a onClick={(e) => props.toggleCollapse(e, i)}>{item.title}</a></h3>
+				            	<div className="icon col-3 text-right"><Icon name={item.icon} size="2x" /></div>
+				            	<div className="col-9 col-lg-8">
+				            		<h4 className="title"><a onClick={(e) => props.toggleCollapse(e, i)}>{item.title}</a></h4>
 				            	</div>
 				        	</div>
 					        <Collapse isOpen={item.expanded}>
 					        	<div className="row">
-					            	<div className="descr col-9 offset-2 col-sm-7 offset-sm-3"><p>{item.text}</p></div>
+					            	<div className="descr col-9 offset-3 col-lg-7"><p>{item.text}</p></div>
 					        	</div>
 					        </Collapse>
 					    </div>
