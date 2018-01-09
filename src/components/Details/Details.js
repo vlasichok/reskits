372030b98e2cd61fc1df9fb03757fe2ccfc6db75
e.rozhko <e.rozhko@admins-iMac.local>
2018-01-09 16:28:29 +0200

    improve - Details section layout

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -9,12 +9,12 @@ const Details = (props) => {
 	return( 
 		<div className="container-fluid details">
 			<div className="row">
-				<div className="col-md-3 col-lg-4 col-xl-5 bgr-snake d-none d-md-block"></div>
-				<div className="col-12 col-md-9 col-lg-8 col-xl-7 bgr-color d-flex align-items-center">
+				<div className="col-lg-4 col-xl-5 bgr-snake d-none d-lg-block"></div>
+				<div className="col-12 col-lg-8 col-xl-7 bgr-color d-flex align-items-center">
 					<div className="details-list w-100">
 						{props.items.map((item, i) => {
 							return(
-							    <div key={i} className="col-12 col-lg-12 mt-2">
+							    <div key={i} className="col-12 col-md-10 offset-md-1 col-lg-12 mt-2">
 						        	<div className="row mt-2">
 						            	<div className="icon col-3 col-md-2 col-lg-1 text-right"><Icon name={item.icon} size="2x" /></div>
 						            	<div className="col-9 col-md-10 col-xl-8">
