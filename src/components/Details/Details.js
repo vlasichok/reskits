09cd9ae030f923f563dd22cda97c7a43f37b3ad5
e.rozhko <e.rozhko@admins-iMac.local>
2018-01-09 10:33:44 +0200

    improve - Details section layout

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -9,21 +9,21 @@ const Details = (props) => {
 	return( 
 		<div className="container-fluid details">
 			<div className="row">
-				<div className="col-md-3 col-lg-4 bgr-snake d-none d-md-block"></div>
-				<div className="col-12 col-md-9 col-lg-8 bgr-color d-flex align-items-center">
+				<div className="col-md-3 col-lg-4 col-xl-5 bgr-snake d-none d-md-block"></div>
+				<div className="col-12 col-md-9 col-lg-8 col-xl-7 bgr-color d-flex align-items-center">
 					<div className="details-list w-100">
 						{props.items.map((item, i) => {
 							return(
-							    <div key={i} className="col-12 col-lg-10 mt-2">
+							    <div key={i} className="col-12 col-lg-12 mt-2">
 						        	<div className="row mt-2">
-						            	<div className="icon col-3 col-lg-1 text-right"><Icon name={item.icon} size="2x" /></div>
-						            	<div className="col-9 col-lg-8">
+						            	<div className="icon col-3 col-md-2 col-lg-1 text-right"><Icon name={item.icon} size="2x" /></div>
+						            	<div className="col-9 col-md-10 col-xl-8">
 						            		<h4 className="title"><a onClick={(e) => props.toggleCollapse(e, i)}>{item.title}</a></h4>
 						            	</div>
 						        	</div>
 							        <Collapse isOpen={item.expanded}>
 							        	<div className="row">
-							            	<div className="descr col-9 offset-3 col-lg-7 offset-lg-1"><p>{item.text}</p></div>
+							            	<div className="descr col-9 offset-3 col-md-10 offset-md-2 col-xl-7 offset-lg-1"><p>{item.text}</p></div>
 							        	</div>
 							        </Collapse>
 							    </div>
