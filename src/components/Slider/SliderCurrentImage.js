ad98eb3ac92b3fdfc3fa74eeffc73896181b8ae3
e.rozhko <e.rozhko@admins-iMac.local>
2018-01-11 15:23:57 +0200

    add - Slider images preloader on desctop

--- src/components/Slider/SliderCurrentImage.js
+++ src/components/Slider/SliderCurrentImage.js
@@ -7,8 +7,14 @@ const Image = props => {
 	let currentImg = currentGallery.imgs[currentGallery.current]
 
 	return(
-		<div className="w-100">
-			<img className="image" src={'/img/packs/250x250/' + currentImg} alt="medkit"/>
+		<div className="image-wrapper w-100">
+			<div className="image-underlay d-flex align-items-center justify-content-center">
+				<Icon spin name="spinner" className="spinner" />
+			</div>
+			<div className="image">
+				<img src={'/img/packs/250x250/' + currentImg} key={currentImg} alt="medkit"/>
+			</div>
+
 			<div className="w-100 text-center">
 				<a onClick={() => props.goToNextImg(props.currentIndex, true)} className="mx-2 no-decoration"><Icon name="arrow-left" /></a>
 				<span>{currentGallery.current+1} / {currentGallery.imgs.length}</span>
