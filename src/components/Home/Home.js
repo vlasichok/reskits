f8525befd6a7416f062a2fec1953c37fe21ba49a
e.rozhko <e.rozhko@admins-iMac.local>
2018-01-11 19:01:28 +0200

    improve - Home section text

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
 		<div className="container home">
 			<div className="row mb-2">
 				<div className="col-12 col-md-10 col-lg-8 offset-md-1 offset-lg-2">
-					<p className="slogan text-center"><span className="title h2">Reskits — современные наборы первой помощи, разработанные для жителей крупных городов.</span> Набор поможет тебе, коллеге или другу спасти жизнь в экстренной ситуации. Выбери свой Reskit!</p>
+					<p className="slogan text-center"><span className="title h2">Reskits — современные наборы первой помощи для жителей крупных городов.</span> Набор поможет тебе, коллеге или другу спасти жизнь в экстренной ситуации. Выбери свой Reskit!</p>
 					<div className="text-center mt-4">
 						<a href='#catalog' className="no-decoration"><button className="btn btn-light m-1">Смотреть каталог</button></a>
 						<a href='#test' className="no-decoration"><button className="btn btn-light m-1">Подобрать аптечку</button></a>
